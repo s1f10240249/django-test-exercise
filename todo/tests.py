676e2eb6852e3a35fd4a02253fad678822e193c1
1F10240249 <s1f102402495@iniad.org>
2025-07-09 13:32:52 +0900

    Taskモデルにメソッドを追加

--- todo/tests.py
+++ todo/tests.py
@@ -5,10 +5,10 @@ from todo.models import Task
 
 # Create your tests here.
 class SampleTestCase(TestCase):
-    def test_sample1(self):
+    def test_sample(self):
         self.assertEqual(1 + 2, 3)
 
-class TaskModelTestCase (TestCase):
+class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         task = Task(title='task1', due_at=due)
@@ -20,10 +20,44 @@ class TaskModelTestCase (TestCase):
         self.assertEqual(task.due_at, due)
 
     def test_create_task2(self):
-        task= Task(title='task2')
+        task = Task(title='task2')
         task.save()
-        
+
         task = Task.objects.get(pk=task.pk)
         self.assertEqual(task.title, 'task2')
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, None)
+
+class Task(models.Model):
+    title = models. CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    posted_at = models.DateTimeField(default-timezone.now)
+    due_at = models.DateTimeField(null=True, blank=True)
+   
+    def is_overdue(self, dt):
+        if self.due_at is None:
+            return False
+        return self.due_at < dt
+
+    def test_is_overdue_future(self):
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
+        task = Task(title='task1', due_at=due)
+        task.save()
+
+        self.assertFalse(task.is_overdue(current))
+    
+    def test_is_overdue_past(self):
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title='task2', due_at=due)
+        task.save()
+
+        self.assertTrue(task.is_overdue(current))
+
+    def test_is_overdue_none(self):
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title='task3')
+        task.save()
+
+        self.assertFalse(task.is_overdue(current))

a43bdc5140da2767b6d4c896ac7882690f181efb
1F10240249 <s1f102402495@iniad.org>
2025-07-09 13:58:44 +0900

    ソート機能を追加

--- todo/views.py
+++ todo/views.py
@@ -10,7 +10,12 @@ def index(request):
                     due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
 
-    tasks = Task.objects.all()
+    tasks Task.objects.order_by('due_at')
+
+    if request.GET.get('order') == 'due':
+        tasks = Task.objects.order_by('due_at')
+    else:
+        tasks = Task.objects.order_by('-posted_at')
 
     context = {
         'tasks': tasks
